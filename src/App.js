0db57e50d17f4417b11c2ce72d815df8f430e538
mmboyce <mathieumimmsboyce@gmail.com>
2020-10-28 13:03:19 -0400

    Refactor backend wake request.
    The backend request has been refactored to be a self-invoking function, and it now includes no-cache headers.

--- src/App.js
+++ src/App.js
@@ -12,8 +12,16 @@ import Results from './components/results/Results';
 
 const apiPath = 'http://192.168.1.226:3000';
 
-// wake Heroku server if it's asleep
-fetch(`${apiPath}`, { mode: 'cors' });
+// The server sleeps after 30min of inactivity, so to ensure the API responses are
+// quick, we ping the server. By pinging early, if the server is asleep it will be
+// awake in time for the client's requests.
+(function wakeServer() {
+  const noCacheHeaders = new Headers();
+  noCacheHeaders.append('pragma', 'no-cache');
+  noCacheHeaders.append('cache-control', 'no-cache');
+
+  fetch(`${apiPath}`, { mode: 'cors', headers: noCacheHeaders });
+}());
 
 function App() {
   return (
