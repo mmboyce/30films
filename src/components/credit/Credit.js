0dc49dd3546cc2ff3f8a0789e8256c85292a4e22
mmboyce <mathieumimmsboyce@gmail.com>
2020-10-27 23:05:48 -0400

    Resize Credit component to match other Components.

--- src/components/credit/Credit.js
+++ src/components/credit/Credit.js
@@ -10,13 +10,7 @@ export default function Credit() {
   return (
     <Container fluid>
       <Row className="mx-auto">
-        <Col sm={{ span: 2, offset: 2 }} className="tmdb-powered-by py-3 pr-0 pl-3">
-          <p>Powered By:</p>
-          <a href="https://www.themoviedb.org/">
-            <img src={tmdbPrimaryFull} className="tmdb-logo" alt="tmdb" />
-          </a>
-        </Col>
-        <Col sm={{ span: 2, offset: 0 }} className="tmdb-powered-by py-3 pl-3 pr-0">
+        <Col sm={{ span: 3, offset: 2 }} className="tmdb-powered-by py-3 pl-3 pr-0">
           <p>Created By:</p>
           <a href="https://mmboyce.github.io" className="by-me">
             <span>
@@ -27,6 +21,11 @@ export default function Credit() {
               {/* &#8209; = non-breaking hyphen */}
               Mimms&#8209;Boyce
             </span>
+          </a>
+        </Col>
+        <Col sm={{ span: 1, offset: 0 }} className="tmdb-powered-by py-3 pr-0 pl-3">
+          <a href="https://www.themoviedb.org/">
+            <img src={tmdbPrimaryFull} className="tmdb-logo" alt="tmdb" />
           </a>
         </Col>
       </Row>
